d68f1849595fae2c379f3721ec4443c982c73f5f
Wotklaus <nacaizag@uce.edu.ec>
2025-10-29 21:24:55 -0500

    configuracion de contenfull1

--- frontend/src/js/contenfulConfig.js
+++ frontend/src/js/contenfulConfig.js
@@ -1,23 +1,19 @@
 const CONTENTFUL_SPACE_ID = 'v6xedn2etntd';
 const CONTENTFUL_ACCESS_TOKEN = '4_5GCKbBbI-sTuWTQsYf1KYMAEvJhqxZpPaV1Xy6gTk';
 
-// Función global para obtener artículos + assets
 async function fetchArticlesWithAssets() {
   try {
     const res = await fetch(
       `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/entries?access_token=${CONTENTFUL_ACCESS_TOKEN}&content_type=ArticuloPokemon&include=2`
     );
     const data = await res.json();
-    console.log('Contentful response:', data);
 
-    // Construye el diccionario de assets
     const assets = (data.includes && data.includes.Asset)
       ? Object.fromEntries(data.includes.Asset.map(a => [a.sys.id, a]))
       : {};
 
     const items = data.items || [];
     if (!Array.isArray(items)) {
-      console.error("No items array returned from Contentful:", data);
       return [];
     }
 
@@ -39,7 +35,6 @@ async function fetchArticlesWithAssets() {
       };
     });
   } catch (err) {
-    console.error("Fetch Contentful error:", err);
     return [];
   }
 }
